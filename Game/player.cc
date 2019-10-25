a2656077bc9e64e9f97f3c4176197218b2f58e9f
Jaakko Humalajoki <jaakko.humalajoki@tuni.fi>
2019-10-25 14:08:50 +0300

    Player function implementations

--- Game/player.cc
+++ Game/player.cc
@@ -1,8 +1,17 @@
 #include "player.hh"
 
+const Course::ResourceMap startResources = {
+    {Course::BasicResource::NONE, 0},
+    {Course::BasicResource::MONEY, 100},
+    {Course::BasicResource::FOOD, 100},
+    {Course::BasicResource::WOOD, 100},
+    {Course::BasicResource::STONE, 100},
+    {Course::BasicResource::ORE, 100}
+};
+
 Player::Player(const std::string &name,
                const std::vector<std::shared_ptr<Course::GameObject> > objects):
-    Course::PlayerBase(name, objects), _resources({})
+    Course::PlayerBase(name, objects), _resources(startResources)
 {
 
 }
@@ -22,15 +31,41 @@ Course::ResourceMap Player::getResources()
 
 bool Player::canAfford(Course::ResourceMap cost)
 {
+    for (std::pair<Course::BasicResource, int> materialNeed : cost) {
+        int materialOwned = _resources.at(materialNeed.first);
+        if (materialOwned < materialNeed.second) {
+            return false;
+        }
+    }
+
     return true;
 }
 
 bool Player::modifyResource(Course::BasicResource resource, int amount)
 {
-    return true;
+    _resources.at(resource) += amount;
+    if (_resources.at(resource) < 0) {
+        return false;
+    } else {
+        return true;
+    }
 }
 
 bool Player::modifyResources(Course::ResourceMap resources)
 {
-    return true;
+    bool negativeResources = false;
+
+    for (std::pair<Course::BasicResource, int> material : resources) {
+        _resources.at(material.first) += material.second;
+        if (_resources.at(material.first) < 0)
+        {
+            negativeResources = true;
+        }
+    }
+
+    if (negativeResources) {
+        return false;
+    } else {
+        return true;
+    }
 }
