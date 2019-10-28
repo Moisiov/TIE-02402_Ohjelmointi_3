4e7d9f551035c9d2bb9070a3dfcc378738c45974
Valto Moisio <valto.moisio@student.tut.fi>
2019-10-28 18:06:18 +0200

    Added function definitions to ObjectManager class.

--- Game/handlers/objectmanager.cpp
+++ Game/handlers/objectmanager.cpp
@@ -1,26 +1,58 @@
 #include "objectmanager.hh"
 
-ObjectManager::ObjectManager()
+ObjectManager::ObjectManager(int map_size,
+                             std::shared_ptr<Course::iGameEventHandler> gameEventHandler):
+    _gameEventHandler(gameEventHandler),
+    _map_size(map_size)
 {
 
 }
 
-void ObjectManager::addTiles(const std::vector<std::shared_ptr<TileBase> > &tiles)
+void ObjectManager::setGEHandler(
+        std::shared_ptr<Course::iGameEventHandler> gameEventHandler)
 {
+    _gameEventHandler = gameEventHandler;
+}
 
+
+void ObjectManager::addTiles(const std::vector<std::shared_ptr<Course::TileBase> > &tiles)
+{
+    _objects.insert(_objects.end(), tiles.begin(), tiles.end());
+
+    for(int i = 0; i < _map_size; ++i) {
+        std::vector<std::shared_ptr<Course::TileBase>> tilesTemp =
+                std::vector<std::shared_ptr<Course::TileBase>>(tiles.begin() + i*_map_size,
+                                                               tiles.begin() + (i+1)*_map_size-1);
+        _tiles.push_back(tilesTemp);
+    }
 }
 
 std::shared_ptr<Course::TileBase> ObjectManager::getTile(const Course::Coordinate &coordinate)
 {
+    int x = coordinate.x();
+    int y = coordinate.y();
 
+    return _tiles.at(x).at(y);
 }
 
 std::shared_ptr<Course::TileBase> ObjectManager::getTile(const Course::ObjectId &id)
 {
+    std::shared_ptr<Course::TileBase> tile;
 
+    for(int x = 0; x < _map_size; ++x)
+    {
+        for(int y = 0; y < _map_size; ++y)
+            if(_tiles.at(x).at(y)->ID == id)
+            {
+                tile = _tiles.at(x).at(y);
+                break;
+            }
+    }
+
+    return tile;
 }
 
-std::vector<std::shared_ptr<Course::TileBase> > ObjectManager::getTiles(const std::vector<Coordinate> &coordinates)
+std::vector<std::shared_ptr<Course::TileBase> > ObjectManager::getTiles(const std::vector<Course::Coordinate> &coordinates)
 {
 
 }
--- Game/handlers/objectmanager.hh
+++ Game/handlers/objectmanager.hh
@@ -2,6 +2,10 @@
 #define OBJECTMANAGER_HH
 
 #include "interfaces/iobjectmanager.h"
+#include "interfaces/igameeventhandler.h"
+#include "core/coordinate.h"
+#include "core/gameobject.h"
+#include "tiles/tilebase.h"
 
 /**
  * @brief The ObjectManager class handles Course-side code
@@ -10,12 +14,14 @@
 class ObjectManager :  Course::iObjectManager
 {
 public:
-    ObjectManager();
+    ObjectManager(int map_size, std::shared_ptr<Course::iGameEventHandler> gameEventHandler = {});
 
     /**
      * @brief Default destructor.
      */
     virtual ~ObjectManager() = default;
+
+    void setGEHandler(std::shared_ptr<Course::iGameEventHandler> gameEventHandler);
 
     virtual void addTiles(const std::vector<std::shared_ptr<Course::TileBase> > &tiles);
 
@@ -24,10 +30,15 @@ public:
     virtual std::shared_ptr<Course::TileBase> getTile(const Course::ObjectId& id);
 
     virtual std::vector<std::shared_ptr<Course::TileBase>> getTiles(
-            const std::vector<Coordinate>& coordinates);
+            const std::vector<Course::Coordinate>& coordinates);
+
+
 
 private:
+    int _map_size; // The length of one side of a square map
     std::vector<std::shared_ptr<Course::GameObject>> _objects;
+    std::vector<std::vector<std::shared_ptr<Course::TileBase>>> _tiles;
+    std::shared_ptr<Course::iGameEventHandler> _gameEventHandler;
 };
 
 #endif // OBJECTMANAGER_HH
